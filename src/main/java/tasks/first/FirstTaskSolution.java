8f102e2be4df54e59ed3ece1f667a93d2016f7e4
Vivrina <i_videeva@mail.ru>
2021-03-11 23:34:46 +0300

    Added breadthFirst and validateBrackets

--- src/main/java/tasks/first/FirstTaskSolution.java
+++ src/main/java/tasks/first/FirstTaskSolution.java
@@ -1,15 +1,73 @@
 package tasks.first;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 public class FirstTaskSolution implements FirstTask {
     @Override
     public String breadthFirst(boolean[][] adjacencyMatrix, int startIndex) {
-        return null;
+        ArrayDeque<Integer> enter = new ArrayDeque<>();
+        enter.addFirst(startIndex);
+        ArrayList<Integer> love = new ArrayList<>();
+
+        for (int i=0; i<adjacencyMatrix.length; i++){
+            for (int j=0; j<adjacencyMatrix.length; j++){
+                if (adjacencyMatrix[enter.getFirst()][j]){
+                    if (!love.contains(j) & !enter.contains(j)){
+                        enter.addLast(j);
+                    }
+                }
+            }
+            love.add(enter.pop());
+
+        }
+
+        String input = love.toString();
+        input = input.substring(1, input.length()-1);
+
+        return input;
+
     }
 
     @Override
     public Boolean validateBrackets(String s) {
-        return null;
+        ArrayDeque<Character> hooks = new ArrayDeque<>();
+
+        for (int i=0; i<s.length(); i++){
+            switch (s.charAt(i)) {
+                case '(':
+                    hooks.addLast(s.charAt(i));
+                    break;
+                case ')':
+                    if (hooks.getLast() == '(') {
+                        hooks.removeLast();
+                    } else return false;
+                    break;
+                case '{':
+                    hooks.addLast(s.charAt(i));
+                    break;
+                case '}':
+                    if (hooks.getLast() == '{') {
+                        hooks.removeLast();
+                    } else return false;
+                    break;
+                case '[':
+                    hooks.addLast(s.charAt(i));
+                    break;
+                case ']':
+                    if (hooks.getLast() == '[') {
+                        hooks.removeLast();
+                    } else return false;
+                    break;
+
+            }
+
+        }
+
+        return  true;
     }
+
+
 
     @Override
     public Long polishCalculation(String s) {
