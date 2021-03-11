e1b8dcb1a7e8bee48e25d20152066e05787a5256
Vivrina <i_videeva@mail.ru>
2021-03-12 00:00:18 +0300

    Added

--- src/main/java/tasks/first/FirstTaskSolution.java
+++ src/main/java/tasks/first/FirstTaskSolution.java
@@ -71,6 +71,59 @@ public class FirstTaskSolution implements FirstTask {
 
     @Override
     public Long polishCalculation(String s) {
-        return null;
+        ArrayDeque<Long> numbers = new ArrayDeque<>();
+        String temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) >= 48) && (s.charAt(i) <= 57)) {
+                temp = temp + s.charAt(i);
+            }
+            else if ((s.charAt(i) == 32)){
+                if((s.charAt(i-1) >= 48) && (s.charAt(i-1) <= 57)) {
+                    putNumber(temp, numbers);
+                    temp = "";
+                }
+            } else {
+                calculate(s.charAt(i), numbers);
+                temp = "";
+            }
+        }
+        return numbers.pop();
+    }
+
+    public void putNumber(String temp, ArrayDeque<Long> nums){
+        long number = 0;
+        long k = 1;
+        for(int i = temp.length()-1; i >= 0; i--){
+            number = number + (temp.charAt(i)-48)*k;
+            k=k*10;
+        }
+        nums.addFirst(number);
+    }
+
+    public void calculate(char op, ArrayDeque<Long> numbers){
+        long a = numbers.pop();
+        long b = numbers.pop();
+        switch(op){
+            case('+'):{
+                long c = a+b;
+                numbers.addFirst(c);
+                break;
+            }
+            case('-'):{
+                long c = a-b;
+                numbers.addFirst(c);
+                break;
+            }
+            case('*'):{
+                long c = a*b;
+                numbers.addFirst(c);
+                break;
+            }
+            case('/'):{
+                long c = a/b;
+                numbers.addFirst(c);
+                break;
+            }
+        }
     }
 }
